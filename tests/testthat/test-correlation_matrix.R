9474a6b1450e904648b53853e86a9dab50361e66
lauralambert99 <laycocla@jmu.edu>
2022-09-06 12:36:32 -0400

    Test - comparing matrices

--- tests/testthat/test-correlation_matrix.R
+++ tests/testthat/test-correlation_matrix.R
@@ -2,7 +2,7 @@ test_that("correlation_matrix creates a matrix", {
   df = data.frame(
     mtcars$mpg, mtcars$cyl)
 
-  expect_equal(is.matrix(correlation_matrix(df)), 'TRUE')
+  expect_equal(is.matrix(correlation_matrix(df)), TRUE)
 
   })
 
@@ -11,9 +11,15 @@ test_that("Correlation matrix makes expected matrix", {
   df = data.frame(
     mtcars$mpg, mtcars$cyl)
 
-  mat_test <- matrix(c("1.00", "-0.85", "-0.85", "1.00"), nrow = 2, ncol = 2)
+  rnames <- c("mtcars.mpg", "mtcars.cyl")
+  cnames <- c("mtcars.mpg", "mtcars.cyl")
+  mat_test <- matrix(c("1.000", "-0.852***", "-0.852***", "1.000"), nrow = 2, ncol = 2,
+                     dimnames = list(rnames,cnames))
 
-  expect_equal(correlation_matrix(df), mat_test)
+  withr::local_options(width = 20)
+  expect_snapshot(
+    waldo::compare(correlation_matrix(df), mat_test)
+  )
 })
 
 
