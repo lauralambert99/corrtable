781f1135ee972713e8717df5ee8246bf0207ca86
lauralambert99 <laycocla@jmu.edu>
2022-09-06 10:22:56 -0400

    Test - matrix equivalencies

--- tests/testthat/test-correlation_matrix.R
+++ tests/testthat/test-correlation_matrix.R
@@ -1,9 +1,19 @@
-test_that("correlation_matrix creates a table", {
+test_that("correlation_matrix creates a matrix", {
   df = data.frame(
     mtcars$mpg, mtcars$cyl)
 
   expect_equal(is.matrix(correlation_matrix(df)), 'TRUE')
 
+  })
+
+
+test_that("Correlation matrix makes expected matrix", {
+  df = data.frame(
+    mtcars$mpg, mtcars$cyl)
+
+  mat_test <- matrix(c("1.00", "-0.85", "-0.85", "1.00"), nrow = 2, ncol = 2)
+
+  expect_equal(correlation_matrix(df), mat_test)
 })
 
 
